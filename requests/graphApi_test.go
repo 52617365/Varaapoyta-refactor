eec9d2f756ab2b865c895cf97d5cc481ae31e061
52617365 <rasengithub@protonmail.com>
2022-12-10 14:46:19 +0200

    Added tests to construct graph api urls.

--- requests/graphApi_test.go
+++ requests/graphApi_test.go
@@ -1,1 +1,35 @@
 package requests
+
+import "testing"
+
+func TestGetUrl(t *testing.T) {
+	restaurantId := 1
+	timeSlot := "0800"
+	expectedUrl := "https://s-varaukset.fi/api/recommendations/slot/1/TODO/0800/1"
+	actualUrl := getUrl(restaurantId, timeSlot)
+	if actualUrl != expectedUrl {
+		t.Errorf("getUrl - Expected %s, got %s", expectedUrl, actualUrl)
+	}
+}
+
+func TestGetUrls(t *testing.T) {
+	restaurantId := 1
+	expectedUrls := []string{
+		"https://s-varaukset.fi/api/recommendations/slot/1/TODO/0800/1",
+		"https://s-varaukset.fi/api/recommendations/slot/1/TODO/1200/1",
+		"https://s-varaukset.fi/api/recommendations/slot/1/TODO/1600/1",
+		"https://s-varaukset.fi/api/recommendations/slot/1/TODO/2000/1",
+	}
+	actualUrls := getUrls(restaurantId)
+
+	if len(actualUrls) != len(expectedUrls) {
+		t.Errorf("getUrls - Expected %d urls, got %d", len(expectedUrls), len(actualUrls))
+	}
+
+	for i, actualUrl := range actualUrls {
+		expectedUrl := expectedUrls[i]
+		if actualUrl != expectedUrl {
+			t.Errorf("getUrls - Expected %s, got %s", expectedUrl, actualUrl)
+		}
+	}
+}
