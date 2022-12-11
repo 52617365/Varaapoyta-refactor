2a0d6b9f6b8894a35f39c728e7e3cd368a62365c
52617365 <rasengithub@protonmail.com>
2022-12-11 12:09:28 +0200

    Added function(s) to restaurantApi.go that handle setting the reservation page id's into the restaurants.
    The reservation page is not the same as the id returned from the endpoint.

--- requests/restaurantApi.go
+++ requests/restaurantApi.go
@@ -3,6 +3,8 @@ package requests
 import (
 	"errors"
 	"net/http"
+	"regexp"
+	"strings"
 	"varaapoyta-backend-refactor/responseStructures"
 )
 
@@ -24,6 +26,7 @@ func GetRestaurants() (*responseStructures.RestaurantApiResponse, error) {
 	if err != nil {
 		return &responseStructures.RestaurantApiResponse{}, err
 	}
+	setReservationIdsToRestaurants(response)
 	return response, nil
 }
 
@@ -52,4 +55,40 @@ func deserializeRestaurantApiResponse(response []byte) (*responseStructures.Rest
 	}
 	result := deserializedResponse.(*responseStructures.RestaurantApiResponse)
 	return result, nil
+}
+
+var RegexToMatchRestaurantId = regexp.MustCompile(`fi/(\d+)`)
+
+// setReservationIdsToRestaurants the id returned from the endpoint is not the same as the one in the reservation page url.
+// we need the latter to access the graph api.
+func setReservationIdsToRestaurants(restaurants *responseStructures.RestaurantApiResponse) {
+	for _, restaurant := range restaurants.Data.ListRestaurantsByLocation.Edges {
+		reservationPageUrl := restaurant.Links.TableReservationLocalized.FiFI
+		if reservationPageExists(reservationPageUrl) {
+			restaurantId, err := getReservationIdFrom(reservationPageUrl)
+			if err != nil {
+				// TODO: this should be logged because it should not have a problem finding the id after the check.
+				continue
+			}
+			restaurant.ReservationPageID = restaurantId
+		}
+	}
+}
+
+func getReservationIdFrom(reservationPageUrl string) (string, error) {
+	restaurantIdMatch := RegexToMatchRestaurantId.FindAllStringSubmatch(reservationPageUrl, -1)
+	if restaurantIdMatch == nil {
+		return "", errors.New("getReservationIdFrom - could not get restaurant id from reservation page url")
+	}
+	restaurantId := restaurantIdMatch[0][1]
+	return restaurantId, nil
+}
+func reservationPageExists(reservationPageUrl string) bool {
+	if reservationPageUrl == "" {
+		return false
+	}
+	if !strings.Contains(reservationPageUrl, "https://s-varaukset.fi/online/reservation/fi/") {
+		return false
+	}
+	return true
 }
