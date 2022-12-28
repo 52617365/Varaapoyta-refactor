ee67db767fe34e76f1b97efccbd2291716225c67
52617365 <rasengithub@protonmail.com>
2022-12-28 14:53:32 +0200

    changed timestamp end in main.go

--- main.go
+++ main.go
@@ -10,8 +10,8 @@ func main() {
 	// benchmark this code
 	t := time.Now()
 	restaurants, _ := requests.GetRestaurantsWithTimeSlots("Helsinki")
+	fmt.Println(time.Since(t))
 	for _, restaurant := range restaurants {
 		fmt.Println(restaurant)
 	}
-	fmt.Println(time.Since(t))
 }
