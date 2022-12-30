67d163f3e5e144731e555b2341de8965b447d14e
52617365 <rasengithub@protonmail.com>
2022-12-30 11:57:23 +0200

    Deleted TODO.

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"varaapoyta-backend-refactor/endpoints"
 )
 
-// TODO: the program terminates because of a out of range index if it's used with "Helsinki", we are for sure missing some potential check that can only happen with restaurants from Helsinki.
 func main() {
     endpoints.InitApi()
 }
